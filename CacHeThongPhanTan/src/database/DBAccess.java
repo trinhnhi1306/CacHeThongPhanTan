37278d156e25d67e318fb8a6bb104882c8491480
trinhnhi1306 <yayoi1306@gmail.com>
2021-10-12 21:30:10 +0700

    Chỉ mới có client

---
+++ CacHeThongPhanTan/src/database/DBAccess.java
@@ -0,0 +1,40 @@
+package database;
+
+import java.sql.*;
+
+public class DBAccess {
+
+    private Connection con;
+    private Statement stmt;
+
+    public DBAccess() {
+        try {
+            // connect with SQL Server
+            SQLServer_Connection myCon = new SQLServer_Connection();
+            con = myCon.getConnection();
+            stmt = con.createStatement();
+        } catch (Exception e) {
+
+        }
+    }
+
+    public int Update(String str) {
+        try {
+            int i = stmt.executeUpdate(str);
+            System.out.println(str);
+            return i;
+        } catch (Exception e) {
+            return -1;
+        }
+    }
+
+    public ResultSet Query(String str) {
+        try {
+            ResultSet rs = stmt.executeQuery(str);
+            System.out.println(str);
+            return rs;
+        } catch (Exception e) {
+            return null;
+        }
+    }
+}
